02031d6a830e5d857e3b0fb2a05d88e6a37e5174
Vignesh-yadav <139734601+Vignesh-yadav@users.noreply.github.com>
2024-01-02 06:52:01 +0530

    wiki API scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
 const userInput = document.getElementById('user-input');
 const chatBox = document.getElementById('chat-box');
 
-// Welcome messages
 const welcomeMessages = [
     "Hello! I'm your friendly chatbot. How can I help you today?",
     "Welcome! Ask me anything and I'll do my best to assist you.",
     "Hi there! Ready to chat. What's on your mind?"
 ];
 
-// Frequently asked questions and corresponding answers
+
 const faq = {
     "What is your name?": "I'm a chatbot.",
     "How are you?": "I'm just a computer program, so I don't have feelings, but thanks for asking!",
-    "What can you do?": "I can provide information, answer questions, or just chat with you. Feel free to ask anything!",
-    // Add more FAQ pairs as needed
+    "What can you do?": "I can provide information, answer questions, or just chat with you. Feel free to ask anything!"
 };
 
-// Greeting messages
+const favourite = [
+    "as i am a chat-bot i don't have any favourites.....",
+    "i don't have feelings so i don't have any favourites,likes or dislikes",
+];
+
 const greetingMessages = [
     "Hi!",
     "Hello!",
@@ -25,13 +27,6 @@ const greetingMessages = [
     "Good to see you!",
 ];
 
-// const cmrinfo = [
-//     "where is hod cabin?": "I'm a chatbot.",
-//     "How are you?": "I'm just a computer program, so I don't have feelings, but thanks for asking!",
-//     "What can you do?": "I can provide information, answer questions, or just chat with you. Feel free to ask anything!",
-// ];
-
-// Additional responses for BTech 1st-year student-related questions
 const studentResponses = [
     "How's your first year going so far?",
     "Need help with any specific subjects in your BTech 1st year?",
@@ -45,27 +40,47 @@ const studentResponses = [
     "Reflecting on the importance of teamwork in engineering projects?",
 ];
 
-//developer questions
+
 const developer = [
     "my developers are 6 btech students pursuing their btech in CMR Engineering College",
     "i've been developed as a project",
 ];
 
-//goodtohear
+const cmr = [
+    "CMR engineering college is one of the top premier private Engineering colleges in Hyderabad spreads over the vast area of 10 acres. The CMR College is authorized under All India Council for Technical Education (AICTE), New Delhi and affiliated to JNTUH.With the commencement of CMR Hyderabad, Our College hit the list of 100 top engineering colleges in India.  In the further journey, the college is also rated 5 Star under Institution Innovation Council, Ministry of Education, Govt of India & Achieved ARIIA Ranking. With these achievements, today, CMR Engineering College is known as one of the Top 10 Engineering Colleges in Hyderabad."
+];
+
 const goodToHear = [
     "good to hear",
     "sounds good",
 ];
 
-//haa
+const cmrprincipal = [
+    "Dr. A. S. Reddy"
+];
+
+lol =[
+    "glad i made you laugh",
+    "haha good to see you smile",
+    "thats funny"
+];
+
 const haa = [
     "ok",
     "okay",
 ];
+const aooffice = [
+    "in cmrec ao office is on the left side of the entrance in the block-I"
+];
+
+const exambranch = [
+    "in cmrec examination branch is on the right side of the entrance in the block-I"
+];
+
+const capabilities = ["you can ask me about CMR ENGINEERING College","i can solve basic maths use solve keyword.","i can give you wikipedia articles use keyword wiki"];
 
 aaaa =["i'm developed by 6 engineers 1.kunta vignesh 2.jadala shivamani 3.cotti manju 4.kunja srija 5.shaik nazeer 6.nanda kumar",];
 
-// Wishes
 const wishes = [
     "Have a great day!",
     "Wishing you all the best!",
@@ -74,52 +89,17 @@ const wishes = [
     "Hope you have an amazing day!",
 ];
 
-console.log("helo world");
-function makeWikipediaRequest(searchTerm) {
-    const wikipediaBaseUrl = 'https://en.wikipedia.org/wiki/';
-    const sanitizedSearchTerm = encodeURIComponent(searchTerm);
-    const wikipediaUrl = wikipediaBaseUrl + sanitizedSearchTerm;
-
-    // Append the Wikipedia link to the chat
-    appendMessage(`<a href="${wikipediaUrl}" target="_blank">${searchTerm}</a>`, 'bot-message');
-}
-
-async function makeWikipediaApiRequest(searchTerm) {
-    const sanitizedSearchTerm = encodeURIComponent(searchTerm);
-    const wikipediaUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&titles=${sanitizedSearchTerm}&prop=extracts&exintro=1&origin=*`;
-
-    try {
-        const response = await fetch(wikipediaUrl);
-        const data = await response.json();
-
-        // Get the first page ID in the response
-        const pageId = Object.keys(data.query.pages)[0];
-
-        // Check if the page ID is valid and has extract data
-        if (pageId && data.query.pages[pageId].extract) {
-            appendMessage(data.query.pages[pageId].extract, 'bot-message');
-        } else {
-            appendMessage('No information found on Wikipedia.', 'bot-message');
-        }
-    } catch (error) {
-        console.error('Error fetching data from Wikipedia API:', error);
-        appendMessage('Sorry, I couldn\'t retrieve information from Wikipedia.', 'bot-message');
-    }
-}
-
 userInput.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         sendMessage();
     }
 });
 
-function sendMessage() {
+async function sendMessage() {
     const userMessage = userInput.value.trim();
     
     if (userMessage !== '') {
         appendMessage(userMessage, 'user-message');
-
-        // Check if the user's message matches a frequently asked question
         const faqAnswer = faq[userMessage];
         if (faqAnswer) {
             setTimeout(() => appendMessage(faqAnswer, 'bot-message'), 500);
@@ -127,12 +107,10 @@ function sendMessage() {
             return;
         }
 
-        // Check for specific keywords to generate responses
+  
         if (containsKeywords(userMessage, ['hello', 'hi', 'hey'])) {
             setTimeout(() => appendRandomMessage(greetingMessages, 'bot-message'), 500);
-        }/* else if (containsKeywords(userMessage, ['how', 'are', 'you'])) {
-            setTimeout(() => appendMessage("I'm just a computer program, so I don't have feelings, but thanks for asking!", 'bot-message'), 500);
-        } */else if (containsKeywords(userMessage, ['bye', 'goodbye'])) {
+        }else if (containsKeywords(userMessage, ['bye', 'goodbye'])) {
             setTimeout(() => appendRandomMessage(wishes, 'bot-message'), 500);
         } else if (containsKeywords(userMessage, ['btech', 'first year','college','student'])) {
             setTimeout(() => appendRandomMessage(studentResponses, 'bot-message'), 500);
@@ -142,12 +120,29 @@ function sendMessage() {
             setTimeout(() => appendRandomMessage(goodToHear, 'bot-message'), 500);
         } else if (containsKeywords(userMessage, ['who', 'are','they','their names'])) {
             setTimeout(() => appendRandomMessage(aaaa, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['lol', 'haha','woah','lolll'])) {
+            setTimeout(() => appendRandomMessage(lol, 'bot-message'), 500);
         }
-         else if (containsKeywords(userMessage, ['who', 'made','developed','you','yourself'])) {
+         else if (containsKeywords(userMessage, ['who', 'made','developed','yourself'])) {
             setTimeout(() => appendRandomMessage(developer, 'bot-message'), 500);
-        }  else if (containsKeywords(userMessage, ['math', 'calculate', 'solve'])) {
-            // Handle basic math questions
-            const mathExpression = userMessage.replace(/[^-()\d/*+.]/g, ''); // Remove non-math characters
+        } else if (containsKeywords(userMessage, ['what', 'can','i ask','you'])) {
+                setTimeout(() => appendRandomMessage(capabilities, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['principal of cmrec','cmrec principal'])) {
+            setTimeout(() => appendRandomMessage(cmrprincipal, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['ao office'])) {
+            setTimeout(() => appendRandomMessage(aooffice, 'bot-message'), 500);
+        }else if (containsKeywords(userMessage, ['examination branch'])) {
+            setTimeout(() => appendRandomMessage(exambranch, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['about cmr'])) {
+            setTimeout(() => appendRandomMessage(cmr, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['what', 'can','i ask','you'])) {
+            setTimeout(() => appendRandomMessage(capabilities, 'bot-message'), 500);
+        } else if (containsKeywords(userMessage, ['favourite', 'fav','favourites','like'])) {
+            setTimeout(() => appendRandomMessage(favourite, 'bot-message'), 500);
+        } 
+         else if (containsKeywords(userMessage, ['math', 'calculate', 'solve']))  {
+          
+            const mathExpression = userMessage.replace(/[^-()\d/*+.]/g, ''); 
             try {
                 const result = eval(mathExpression);
                 setTimeout(() => appendMessage(`The result is: ${result}`, 'bot-message'), 500);
@@ -155,12 +150,21 @@ function sendMessage() {
                 setTimeout(() => appendMessage('Sorry, I couldn\'t calculate that.', 'bot-message'), 500);
             }
         } else if(containsKeywords(userMessage, ['wiki', 'wikipedia'])) {
-            const searchTerm = userMessage.replace(/[^a-zA-Z0-9 ]/g, ''); // Remove non-alphanumeric characters
-          //  makeWikipediaApiRequest(searchTerm);
-          makeWikipediaRequest(searchTerm);
-        }
+            const searchTerm = userMessage.replace(/[^a-zA-Z0-9 ]/g, '').replace('wiki', '').trim();
+            if (searchTerm !== '')  {
+                const result = await fetchWikipediaData(searchTerm);
+                if (result) {
+                    appendMessage(`Wikipedia: ${result.title}\n${result.extract}`, 'bot-message');
+                } else {
+                    appendMessage("Article not found on Wikipedia", 'bot-message');
+                }
+                userInput.value = '';
+                return;
+            }
+
+       
+    }
          else {
-            // Default response for unrecognized input
             setTimeout(() => appendMessage("I'm not sure how to respond to that. Feel free to ask something else!", 'bot-message'), 500);
         }
 
@@ -185,5 +189,4 @@ function containsKeywords(message, keywords) {
     return keywords.some(keyword => message.toLowerCase().includes(keyword));
 }
 
-// Initial welcome message
 setTimeout(() => appendRandomMessage(welcomeMessages, 'bot-message'), 500);
